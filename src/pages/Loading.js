039ac07491b178de93f222391add6a56e8f8d72f
Gleb Myznikov <myznikovgleb@yandex.ru>
2023-05-04 22:34:49 +0300

    Add spiner to `Loading` page

--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -2,7 +2,7 @@
 export default function Loading() {
   return (
     <main className="w-screen h-screen flex flex-col items-center justify-top py-16">
-      <h1 className="text-3xl font-bold">Loading</h1>
+      <div className="w-24 h-24 border-4 rounded-full border-black/0 border-t-black animate-spin"></div>
     </main>
   )
 }
